f5b94849f928610abdcdf62d3baabc85f4fc1d31
Tyler Hedegard <tchedegard@gmail.com>
2016-05-17 18:55:52 +0000

    print customers and bikes they can buy

--- bicycle.py
+++ bicycle.py
@@ -30,7 +30,7 @@ class Customer(object):
         self.bicycle.append(Bicycle)
         print("{} now own {}.".format(self.name,self.bicycle))
         
-if __name__ == "__main__":
+def createData():
     ricks = BicycleShop("Rick's Bike Shop",0.2)
     bmx = Bicycle("BMX",100,150)
     tenSpeed = Bicycle("10 Speed",100,250)
@@ -38,9 +38,19 @@ if __name__ == "__main__":
     mountain = Bicycle("Ridge Rider",130,350)
     trike = Bicycle("Trike",75,100)
     moped = Bicycle("MoPed",200,350)
-    chris = Customer("Chris",200)
-    peter = Customer("Peter",500)
-    lila = Customer("Lila",1000)
+
+def options(customer,shop):
+    """Prints out the customer name and the bikes he can afford"""
+    print(customer.name + ", you can buy:")
+    bikes = []
+    for i in shop.inventory:
+        if i.name not in bikes and i.cost * (1+shop.margin) <= customer.budget:
+            bikes.append(i.name)
+            print(i.name + " for $" + str(i.cost*(1+shop.margin)))
+
+if __name__ == "__main__":
+    #Create a bike shop with 6 bike models
+    createData()
     
     for i in range(3):
         ricks.addInventory(bmx)
@@ -50,6 +60,15 @@ if __name__ == "__main__":
         ricks.addInventory(trike)
         ricks.addInventory(moped)
     
-    print(len(ricks.inventory))
-    print(ricks.margin)
-    print(ricks.inventory[0].name)
+    #Create 3 customers
+    chris = Customer("Chris",200)
+    peter = Customer("Peter",500)
+    lila = Customer("Lila",1000)
+    
+    #Print each customer and the bikes they can buy
+    options(chris,ricks)
+    options(peter,ricks)
+    options(lila,ricks)
+    
+    #prove functionality
+    
