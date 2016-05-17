3a461fb837af1f09866ee62d216a318f4255bd56
Tyler Hedegard <tchedegard@gmail.com>
2016-05-17 14:23:42 +0000

    classes made

--- bicycle.py
+++ bicycle.py
@@ -2,3 +2,22 @@
 # Thinkful.com Python Introduction Lesson 3.4
 # Bicycle Assignment
 
+class Bicycle(name,weight,cost):
+    def __init__(self,name,weight,cost):
+        self.name = name
+        self.weight = weight
+        self.cost = cost
+        
+class BicycleShop(name):
+    def __init__(self.name):
+        self.name = name
+        self.margin = 0.2
+        self.profit = 0
+        self.inventory = []
+
+class Customer(name,budget):
+    def __init(self,name,budget):
+        self.name = name
+        self.budget = budget
+        self.bicycle = []
+        
